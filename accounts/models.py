4e0386efb7bf3fa7879474d2916a2f6a3597a7d3
Lauren <lauren.main28@gmail.com>
2023-06-26 21:33:18 -0700

    adds styling to login pages

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 from django.contrib.auth.models import AbstractUser
 
 
@@ -8,6 +9,7 @@ class CustomUser(AbstractUser):
     username = models.CharField(max_length=50, unique=True)
     age = models.IntegerField(null=True)
     birthday = models.DateField(null=True)
+    # password = models.CharField(max_length=100)
 
 
     def __str__(self):
